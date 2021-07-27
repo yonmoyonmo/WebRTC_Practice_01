d428e0e908732a944dc10ae34dd38f99e6baaee2
yonmoyonmo <yonmosound@gmail.com>
2021-07-27 20:05:07 +0900

    dd

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
-
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
